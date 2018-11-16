e1606d97d0f9d7c2647734881a9ea09272cb64a6
Alexandr <34200871+AlexVainer@users.noreply.github.com>
2018-11-16 15:52:51 +0300

    Update sendsms.js

--- middlewares/sendsms.js
+++ middlewares/sendsms.js
@@ -1,7 +1,7 @@
 const SMSru = require('sms_ru');
 const sms = new SMSru('7113B561-1A53-3E9D-D3D2-DCB1BCC13819');
 
-let sendSms = () =>{
+let sendSms = (req) =>{
   sms.sms_send({
     to: req.body.phone,
     text: `Привет ${req.body.name}! Ты был успешно зарегестрирован на JetLend` 
